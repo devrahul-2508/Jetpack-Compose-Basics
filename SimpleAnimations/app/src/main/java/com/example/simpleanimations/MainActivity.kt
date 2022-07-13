357160d0f586a42bc5dc7e9ff54d48ea89478af3
Rahul Roy <devrahul2508@gmail.com>
2022-07-13 22:23:59 +0530

    Animation

--- SimpleAnimations/app/src/main/java/com/example/simpleanimations/MainActivity.kt
+++ SimpleAnimations/app/src/main/java/com/example/simpleanimations/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.simpleanimations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -24,7 +26,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Animation1()
         }
     }
 }
@@ -34,9 +36,41 @@ fun Animation1(){
     var sizeState by remember{
         mutableStateOf(100.dp)
     }
+    val size by animateDpAsState(targetValue = sizeState,
+    tween(
+        durationMillis = 3000,
+
+    )
+
+   /* spring(
+        Spring.DampingRatioHighBouncy
+    ))*/
+   /*keyframes {
+       durationMillis = 5000
+       sizeState at 0 with LinearEasing
+       sizeState * 1.5f at 1000 with FastOutLinearInEasing
+       sizeState * 2f at 5000
+   }*/
+
+    )
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Red ,
+        targetValue = Color.Green,
+        animationSpec = infiniteRepeatable(
+            tween(
+                durationMillis = 2000
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+
+
     Box(modifier = Modifier
-        .size(sizeState)
-        .background(Color.Red),
+        .size(size)
+        .background(color),
     contentAlignment = Alignment.Center){
         Button(onClick = {
             sizeState += 50.dp
