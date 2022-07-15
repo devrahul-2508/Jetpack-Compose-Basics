7493430d5eeeb4c758f84feb4373e0b0717416d7
Rahul Roy <devrahul2508@gmail.com>
2022-07-15 12:22:46 +0530

    Features

--- MeditationUI/app/src/main/java/com/example/meditationui/ui/HomeScreen.kt
+++ MeditationUI/app/src/main/java/com/example/meditationui/ui/HomeScreen.kt
@@ -1,19 +1,31 @@
 package com.example.meditationui.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.meditationui.Feature
 import com.example.meditationui.R
-import com.example.meditationui.ui.theme.DeepBlue
+import com.example.meditationui.ui.theme.*
 
 @Composable
 fun HomeScreen() {
@@ -23,7 +35,9 @@ fun HomeScreen() {
             .fillMaxSize()
     ) {
         Column {
-           GreetingSection()
+            GreetingSection()
+            ChipSection(chips = listOf("Sweet Sleep", "Insomnia", "Depression"))
+            CurrentMeditation()
         }
     }
 }
@@ -45,7 +59,7 @@ fun GreetingSection(
         ) {
             Text(
                 text = "Good Morning , $name",
-                style = MaterialTheme.typography.h4,
+                style = MaterialTheme.typography.h6,
                 color = Color.White
             )
             Text(
@@ -63,6 +77,112 @@ fun GreetingSection(
     }
 
 }
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+    LazyRow {
+        items(chips.size) {
+            Box(modifier = Modifier
+                .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                .clickable {
+                    selectedChipIndex = it
+                }
+                .clip(RoundedCornerShape(10.dp))
+                .background(
+                    if (selectedChipIndex == it) ButtonBlue
+                    else DarkerButtonBlue
+                )
+                .padding(15.dp)) {
+                Text(text = chips[it], color = TextWhite)
+            }
+        }
+    }
+}
+
+@Composable
+fun CurrentMeditation(
+    color: Color = LightRed
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .padding(horizontal = 15.dp, vertical = 15.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Daily Thought",
+                style = MaterialTheme.typography.body1,
+                color = Color.White
+            )
+            Text(
+                text = "Meditation for 10-15 mins",
+                style = MaterialTheme.typography.body2,
+                color = Color.White
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .size(45.dp)
+                .clip(CircleShape)
+                .background(ButtonBlue)
+                .padding(10.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_play),
+                contentDescription = "Play",
+                tint = Color.White,
+                modifier = Modifier.size(24.dp)
+            )
+        }
+
+
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun FeaturesSection(
+    features: List<Feature>
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "Features",
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier.padding(15.dp)
+        )
+
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp , bottom = 100.dp),
+            modifier = Modifier.fillMaxHeight()
+        ){
+            items(features.size){
+                
+            }
+        }
+    }
+}
+
+@Composable
+fun FeatureItem() {
+    
+}
+
 
 @Preview
 @Composable
